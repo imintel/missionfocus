c1d736a77c223150046e130fca9b1e904e67ffb8
Helen Holmes <hvholmes@missionfocus.com>
2014-01-02 16:38:06 -0500

    Added jquery mobile icons

--- _site/js/inits.js
+++ _site/js/inits.js
@@ -4,11 +4,28 @@ $( document ).ready(function() {
     $('.nav--index-header-anchors').removeClass('active');
     $(this).addClass('active');
 
-    $("[id$='-body']").hide();
+    $('.nav--index-header-anchors').each(function(){
+        var href = $(this).prop('hash');
+        $(href).hide();
+    });
 
-    var id = $(this).attr('id') + '-body';
-    console.log(id);
-    $('#' + id).show();
+    var id = $(this).prop('hash');
+    $(id).show();
+  });
+
+  ($('.ui-panel-inner a')).click(function(){
+    var href = $(this).prop('hash');
+    if (typeof attr !== 'undefined' && attr !== false) {
+        // Just load page normally
+    } 
+    else {
+      $( '#slidemenupanel' ).panel( 'close' );
+      $('.nav--index-header-anchors').each(function(){
+        var navHref = $(this).prop('hash');
+        $(navHref).hide();
+      });
+      $(href).show();
+    }
   });
 
 });
